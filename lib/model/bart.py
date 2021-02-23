5033039cc9dd137cc53922c513ae9ded6f7e46b1
tree-park <hspark.engineer@gmail.com>
2021-02-23 15:44:46 +0900

    start coding

---
+++ lib/model/bart.py
@@ -0,0 +1,14 @@
+
+import torch
+import torch.nn as nn
+
+
+class BartModel(nn.Module):
+
+    def __init__(self, config: BartConfig):
+        super(BartModel, self).__init__()
+
+    def forward(self):
+        return 0
+
+
---
+++ lib/model/layer.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+
+
+class BartEncoder(nn.Module):
+    def __init__(self):
+        super(BartEncoder, self).__init__()
+
+    def forward(self):
+        return 0
+
+
+class BartDecoder(nn.Module):
+    def __init__(self):
+        super(BartDecoder, self).__init__()
+
+    def forward(self):
+        return 0
+
+
+class BartEncoderLayer(nn.Module):
+    def __init__(self):
+        super(BartEncoderLayer, self).__init__()
+
+    def forward(self):
+        return 0
+
+
+class BartDecoderLayer(nn.Module):
+    def __init__(self):
+        super(BartDecoderLayer, self).__init__()
+
+    def forward(self):
+        return 0
